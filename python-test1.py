4300c3719c139c08df06832670dd19a804f706c8
adarshnamsani <adarsh.namsani@gmail.com>
2016-06-24 16:22:02 +0530

    Adarsh_Python_exam

--- python-test1.py
+++ python-test1.py
@@ -1,11 +1,21 @@
 from placeholders import *
+import math
+from collections import OrderedDict
 
 def get_odds_list(count):
     """
      This method returns a list of the first 'count' odd numbers in descending
      order. e.g count = 3 should return [5,3,1]
     """
-    return None
+    l = []
+    a = count
+    count = count * 2
+    while count > 0:
+        if count % 2 != 0:
+            l.append(count)
+        count -= 1
+
+    return l
 
 
 def get_odd_mountain(count):
@@ -16,7 +26,21 @@ def get_odd_mountain(count):
 
     Hint: use the list functions and a builtin function we have already seen.
     """
-    return None
+    l = []
+    i = 1
+    if count == 0:
+        return l
+    else:
+        while i <= count:
+            if i % 2 != 0:
+                l.append(i)
+            i += 1
+        count -= 1
+        while count > 0:
+             if count % 2 != 0:
+                  l.append(count)
+             count -= 1
+        return l
 
 def get_multiples_desc(number, count):
     """
@@ -26,6 +50,15 @@ def get_multiples_desc(number, count):
 
     Hint: one line of code, use a builtin function we have already seen in the lists lesson.
     """
+    l = []
+    if count == 0:
+        return l
+    else:
+        while count > 0:
+            l.append(number*count)
+            count -= 1
+        return l
+
     return None
 
 def get_sorted_diff_string(first, second):
@@ -33,24 +66,44 @@ def get_sorted_diff_string(first, second):
     returns a string which contains letters in first but not in second.
     e.g.s apple and pig returns ael.
     """
-    return None
+    st1 = set(first)
+    st2 = set(second)
+    st = list(set(st1)-set(st2))
+    st4 = sorted(st)
+    st3 = ''.join(st4)
+    return st3
+
 
 def get_sorted_without_duplicates(input):
     """
     returns a string in which characters are sorted and duplicates removed
     e.g apple returns aelp, engine returns egin
     """
-    return None
+    st1 = set(input)
+    st1 = sorted(st1)
+    st2 = ''.join(st1)
+    return st2
 
 def create_palindrome(word):
-    pass
+    if word == None:
+        return None
+    x = word[::-1]
+    z = word+x
+    return z
 
 
 # Sort the words that are passed in by word length instead of word content.
 # e.g ["apple", "dog", "elephant"] should result in ["elephant", "apple", "dog"]
 # hint: use list.sort, don't write your own
 def sort_by_length(words):
-    pass
+    if words == None:
+        return None
+    else:
+        strs = sorted(words, key = len, reverse=True)
+        return strs
+
+def sample(words):
+    return True
 
 def test_odds_list():
     assert [1] == get_odds_list(1)
@@ -59,42 +112,45 @@ def test_odds_list():
     assert [9,7,5,3,1] == get_odds_list(5)
 
 def test_get_odd_mountain():
-    assert [1,1] == get_odd_mountain(2)
-    assert [1,3,1] == get_odd_mountain(3)
-    assert [1,3,5,7,5,3,1] == get_odd_mountain(7)
-    assert [] == get_odd_mountain(0)
+     assert [1,1] == get_odd_mountain(2)
+     assert [1,3,1] == get_odd_mountain(3)
+     assert [1,3,5,7,5,3,1] == get_odd_mountain(7)
+     assert [] == get_odd_mountain(0)
 
 def test_get_multiples_desc():
-    assert [6,3] == get_multiples_desc(3,2)
-    assert [15, 10, 5] == get_multiples_desc(5,3)
-    assert [] == get_multiples_desc(6, 0)
-    assert [3,2,1] == get_multiples_desc(1, 3)
+     assert [6,3] == get_multiples_desc(3,2)
+     assert [15, 10, 5] == get_multiples_desc(5,3)
+     assert [] == get_multiples_desc(6, 0)
+     assert [3,2,1] == get_multiples_desc(1, 3)
 
 
 def test_sorted_diff_string():
-    assert "" == get_sorted_diff_string("apple", "apple")
-    assert "aelp" == get_sorted_diff_string("apple", "")
-    assert "do" == get_sorted_diff_string("dog", "pig")
-    assert "in" == get_sorted_diff_string("pineapple", "apple")
+     assert "" == get_sorted_diff_string("apple", "apple")
+     assert "aelp" == get_sorted_diff_string("apple", "")
+     assert "do" == get_sorted_diff_string("dog", "pig")
+     assert "in" == get_sorted_diff_string("pineapple", "apple")
 
 
 def test_sorted_without_duplicates():
-    assert "aelp" == get_sorted_without_duplicates("apple")
-    assert "eorz" == get_sorted_without_duplicates("zeroo")
-    assert "" == get_sorted_without_duplicates("")
-    assert "abcd" == get_sorted_without_duplicates("abcdabcd")
+     assert "aelp" == get_sorted_without_duplicates("apple")
+     assert "eorz" == get_sorted_without_duplicates("zeroo")
+     assert "" == get_sorted_without_duplicates("")
+     assert "abcd" == get_sorted_without_duplicates("abcdabcd")
 
 def test_create_palindrome():
-        assert "battab" == create_palindrome("bat")
-        assert "abba" == create_palindrome("ab")
-        assert "" == create_palindrome("")
-        assert None == create_palindrome(None)
+         assert "battab" == create_palindrome("bat")
+         assert "abba" == create_palindrome("ab")
+         assert "" == create_palindrome("")
+         assert None == create_palindrome(None)
 
 def test_sort_by_length():
-    assert ["apple", "bear", "dog"] == sort_by_length(["dog", "apple", "bear"])
-    assert ["apple", "bear", "dog"] == sort_by_length(["apple", "dog",  "bear"])
-    assert ["apple", "dog", "cat"] == sort_by_length(["dog", "apple", "cat"])
-    assert ["elephant", "apple"] == sort_by_length(["apple", "elephant"])
-    assert ["three", "four", "one", "two"] == sort_by_length(["one", "two", "three", "four"])
-    assert [] == sort_by_length([])
-    assert None == sort_by_length(None)
+     assert ["apple", "bear", "dog"] == sort_by_length(["dog", "apple", "bear"])
+     assert ["apple", "bear", "dog"] == sort_by_length(["apple", "dog",  "bear"])
+     assert ["apple", "dog", "cat"] == sort_by_length(["dog", "apple", "cat"])
+     assert ["elephant", "apple"] == sort_by_length(["apple", "elephant"])
+     assert ["three", "four", "one", "two"] == sort_by_length(["one", "two", "three", "four"])
+     assert [] == sort_by_length([])
+     assert None == sort_by_length(None)
+
+def test_sample():
+        assert True == sample("bat")
