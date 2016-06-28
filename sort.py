9cd063ca93abceec5f73dbfba95d47298dfaf016
srinidhik <srinidhi.kambalapally@gmail.com>
2016-06-28 09:29:07 +0530

    sort

--- sort.py
+++ sort.py
@@ -12,8 +12,8 @@ def selection_sort(a):
 def bubble_sort(a):
     for i in range(len(a)- 1,0,-1):
         for j in range(0,i,1):
-            if a[j] > a[i]:
-                a[i],a[j] = a[j],a[i]
+            if a[j] > a[j+1]:
+                a[j+1],a[j] = a[j],a[j+1]
 
     return a
 
