07a5667a4ef496cde9c94b0a962f1e039e98bf0d
sanjay <sanjayhcu01@gmail.com>
2016-08-01 14:07:48 +0530

    1 Add new method to Dictionary                           update values                           add new values to existing key                           get all items  2 Change the method name   3. remove all input from class Dictionary

--- Dictaionary.py
+++ Dictaionary.py
@@ -1,5 +1,5 @@
 class DictionaryItem():
-    def __init__(self,key = None,value = None):
+    def __init__(self,key = None,value = []):
         self.key = key
         self.value = value
 
@@ -8,8 +8,8 @@ class Dictionary():
     def __init__(self,dic = []):
         self.dic = dic
 
-    def create_dic(self):
-        dic_obj = DictionaryItem(raw_input('Enter the key : '), raw_input('Enter the value : '))
+    def add_entry(self,key,value):
+        dic_obj = DictionaryItem(key,value)
         self.dic.append(dic_obj)
 
     def get_value_by_key(self,key):
@@ -23,7 +23,7 @@ class Dictionary():
             if obj.key == key:
                 obj.key = newkey
 
-    def delete_key(self,key):
+    def delete_entry(self,key):
         for obj in self.dic:
             if obj.key == key:
                 self.dic.remove(obj)
@@ -32,53 +32,83 @@ class Dictionary():
         for obj in self.dic:
             li.append(obj.key)
         return li
-    def get_all_value(self):
+    def get_all_values(self):
         li = []
         for obj in self.dic:
             li.append(obj.value)
         return li
-    def get_key_by_value(self,value):
+    def get_keys_by_value(self,value):
         li = []
         for obj in self.dic:
-            if obj.value == value:
+            if value in  obj.value :
                 li.append(obj.key)
         return li
-    def display_dictionary(self):
+    def get_all_items(self):
         str = []
         for obj in self.dic:
             str.append((obj.key,obj.value))
         return str
 
+    def update_value(self,key,value):
+        for obj in self.dic:
+            if obj.key == key:
+                obj.value = value
+                return True
+        return False
+    def add_value_to_existing_key(self,key,value):
+        for obj in self.dic:
+            li = []
+            li.append(value)
+            if obj.key == key:
+                li.append(obj.value)
+            obj.value = li
+
 
 class DictionaryManager():
 
-
     def main(self):
-        print "1.Add new entry !\n2.Get Key by value !\n3.Get value by key !\n4.Update a key !\n5.delete entry from Dictionary! \n6.Get all Keys \n7. Get all values\n9.Display Dictionary\nExit "
+        print "1.Add new entry !\n2.Get Key by value !\n3.Get value by key !\n4.Update a key !\n5.delete entry from Dictionary!" \
+              " \n6.Get all Keys \n7. Get all values\n8.Display Dictionary\nUpdate the value\n10.Update the value\n11. Add new value to existing key\n Exit "
         di = Dictionary()
         while(True):
             choice = raw_input("Enter your Choice : ")
             if choice == '1':
-                di.create_dic()
+                key = raw_input('Enter the key : ')
+                value = raw_input('Enter the value : ')
+                di.add_entry(key,value)
             elif choice == '2':
-                print di.get_key_by_value(raw_input('enter the value : '))
+                print di.get_keys_by_value(raw_input('enter the value : '))
             elif choice == '3':
                 print di.get_value_by_key(raw_input('enter the key : '))
             elif choice == '4':
                 di.update_key(raw_input('enter the existing key : '),raw_input('enter new key : '))
             elif choice == '5':
-                di.delete_key(raw_input('enter the key : '))
+                di.delete_entry(raw_input('enter the key : '))
             elif choice == '6':
                 print di.get_all_keys()
             elif choice == '7':
-                print di.get_all_value()
+                print di.get_all_values()
             elif choice == '8':
-                print di.display_dictionary()
+                print di.get_all_items()
 
             elif choice == '9':
+                key = raw_input('Enter the key : ')
+                newvalue = raw_input('Enter the New value : ')
+                di.update_value(key,newvalue)
+            elif choice == '10':
+                key = raw_input('Enter the key : ')
+                value = raw_input('Enter the new value')
+                di.update_value(key,value)
+            elif choice == '11':
+                key = raw_input('Enter the key : ')
+                value = raw_input('Enter the new value')
+                di.add_value_to_existing_key(key, value)
+            elif choice == '12':
                 return
+
             else:
-                print "Enter The Correct choice\n1.Add new entry !\n2.Get Key by value !\n3.Get value by key !\n4.Update a key !\n5.delete entry from Dictionary! \n6.Get all Keys \n7. Get all values\n9.Display Dictionary\nExit "
+                print "Enter The Correct choice\n1.Add new entry !\n2.Get Key by value !\n3.Get value by key !\n4.Update a key !" \
+                      "\n5.delete entry from Dictionary! \n6.Get all Keys \n7. Get all values\n8.Display Dictionary\n 9. Update the value\nExit "
                # choice = raw_input("Enter your Choice : ")
 
 
